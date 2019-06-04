0dd282e02fc9014f249910a02b665bd709e1a748
cyyzero <cyyzero16@qq.com>
2019-06-04 15:12:24 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 ./main 1 > 1.log 2>&1 &
 ./main 2 > 2.log 2>&1 &
 ./main 3 > 3.log 2>&1 &
-# ./main 4 & > 4.log;
-# ./main 5 & > 5.log;
-# ./main 6 & > 6.log;
-# ./main 7 & > 7.log;
+./main 4 > 4.log 2>&1 &
+./main 5 > 5.log 2>&1 &
+./main 6 > 6.log 2>&1 &
+./main 7 > 7.log 2>&1 &
